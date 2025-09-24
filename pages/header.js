ed9abe3b4a28bceb02f5316377ab2350a3c898d6
TYX8926 <139226237+TYX8926@users.noreply.github.com>
2025-09-24 22:34:42 +0800

    Updated header design

--- pages/header.js
+++ pages/header.js
@@ -3,9 +3,6 @@ document.write('\
     <div id="intro-text">\
       <h2>RetroDoc</h2>\
       <h5>Retrocomputing for beginners and enthusiasts alike</h5>\
-    </div>\
-    \
-    <div id="hp-category">\
       <ul>\
         <li><a href="../index.html">Home</a></li>\
         <li><a href="../test.html">Articles & Pages</a></li>\

3a0d0511ee9692f807434e726f827f8e9cebce84
TYX8926 <139226237+TYX8926@users.noreply.github.com>
2025-09-24 22:05:02 +0800

    Updated homepage, unorganized article + CSS file, and updated JavaScript header file

--- pages/header.js
+++ pages/header.js
@@ -2,14 +2,14 @@
 document.write('\
     <div id="intro-text">\
       <h2>RetroDoc</h2>\
-      <h5>Home of retrocomputing for beginners and enthusiasts alike</h5>\
+      <h5>Retrocomputing for beginners and enthusiasts alike</h5>\
     </div>\
     \
     <div id="hp-category">\
       <ul>\
         <li><a href="../index.html">Home</a></li>\
         <li><a href="../test.html">Articles & Pages</a></li>\
-        <li style="float: right;"><a href="about.html">About RetroDoc</a></li>\
+        <li style="float: right;"><a href="../about.html">About RetroDoc</a></li>\
       </ul>\
     </div>\
 ');

ff9102084ff30eee5b3dc6d2745f42786551f5c5
TYX8926 <139226237+TYX8926@users.noreply.github.com>
2025-09-24 13:06:33 +0800

    Updates to about.html and styles.css, testing JavaScript for the first time, adding new page for my 98 PC build quest

---
+++ pages/header.js
@@ -0,0 +1,7 @@
+document.write('\
+    <div id="intro-text">\
+        <h1 style="color: rgb(135, 206, 250)">RetroDoc</h1>\
+        <p style="font-size: 16px">The home of beginner-friendly & enthusiast-level retrocomputing</p>\
+        <br>\
+    </div>\
+');

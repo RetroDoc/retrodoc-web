07c1f65a0d6b4f50f0b0b966f20a8bda3d74e05b
TYX8926 <139226237+TYX8926@users.noreply.github.com>
2025-09-25 12:50:45 +0800

    Getting all pages

--- resources/pages-list.js
+++ resources/pages-list.js
@@ -1,4 +1,4 @@
-const articlePages = JSON.parse("pages-list.json");
+var articlePages = JSON.parse("pages-list.json");
 
 for (let i = 0; i < articlePages.articles.length; i++) {
     document.write('<a href="' + articlePages.articles[i].actual + '">' + articlePages.articles[i].title + '</a>');

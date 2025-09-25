94315f80e1fc9b89d4cf08d25c338bf5c3013218
TYX8926 <139226237+TYX8926@users.noreply.github.com>
2025-09-25 12:38:00 +0800

    Add pagelist properly (Test 1)

---
+++ resources/pages-list.js
@@ -0,0 +1,5 @@
+const articlePages = JSON.parse("pages-list.json");
+
+for (let i = 0; i < articlePages.articles.length; i++) {
+    document.write('<a href="' + articlePages.articles[i].actual + '">' + articlePages.articles[i].title + '</a>');
+}

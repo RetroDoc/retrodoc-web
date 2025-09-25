545038285d36e2ce1b1d0185ba78be0a98404ecb
TYX8926 <139226237+TYX8926@users.noreply.github.com>
2025-09-25 13:07:34 +0800

    Made a simple page/article list, manual

--- resources/pages-list.js
+++ resources/pages-list.js
@@ -1,5 +1,20 @@
-var articlePages = JSON.parse("pages-list.json");
+var webpageList = [
+    "Quest to Build my First Windows 98 PC",
+    "What is ISA?",
+    "What is DMA?"
+]
 
-for (let i = 0; i < articlePages.articles.length; i++) {
-    document.write('<a href="' + articlePages.articles[i].actual + '">' + articlePages.articles[i].title + '</a>');
+var fileList = [
+    "quest_to_build_my_first_windows_98_pc",
+    "what_is_isa",
+    "what_is_dma"
+]
+
+if (webpageList.length != fileList.length) {
+    console.error("Invalid page list length!");
+}
+else {
+    for (let i = 0; i < webpageList.length; i++) {
+        document.write('<li><a href="' + fileList[i] + '.html">' + webpageList[i] + '</a></li>')
+    } 
 }

5ca90871e0b2cbe5e651694fb6924b82c993e0d3
TYX8926 <139226237+TYX8926@users.noreply.github.com>
2025-10-01 01:36:59 +0800

    Update pages-list.js

--- resources/pages-list.js
+++ resources/pages-list.js
@@ -1,18 +1,3 @@
-const xhr = new XMLHttpRequest();
-    const url = "https://api.github.com/repositories/1039382278/contents/pages"
-    // Replace -username- with your GitHub username, -repo- with the repository name, and then :path with a path to the file or folder you want to get the content of (leave blank to ge all files of the repository)
-
-    xhr.open('GET', url, true);
-
-    xhr.onload = function() {
-        const data = JSON.parse(this.response);
-        console.log(data);
-
-        for (var i = 0; i < data.length; i++) {
-            const page_lister = document.createElement("li");
-            page_lister.innerHTML = '<a href="' + data[i].name + '.html">' + data[i].name + '</a>';
-            document.getElementById("pages-list").appendChild(page_lister);
-        }
-    };
-    
-    xhr.send();
+const page_lister = document.createElement("li");
+page_lister.innerHTML = 'Currently not done yet.';
+document.getElementById("pages-list").appendChild(page_lister);

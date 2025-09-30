151e45e80ff2ffa1544d2d88250c88304b6009ed
TYX8926 <139226237+TYX8926@users.noreply.github.com>
2025-09-30 22:18:08 +0800

    Update pages-list.js

--- resources/pages-list.js
+++ resources/pages-list.js
@@ -1,20 +1,37 @@
-var webpageList = [
-    "Quest to Build my First Windows 98 PC",
-    "What is ISA?",
-    "What is DMA?"
-]
+const xhr = new XMLHttpRequest();
+    const url = "https://api.github.com/repositories/1039382278/contents/pages"
+    // Replace -username- with your GitHub username, -repo- with the repository name, and then :path with a path to the file or folder you want to get the content of (leave blank to ge all files of the repository)
 
-var fileList = [
-    "quest_to_build_my_first_windows_98_pc",
-    "what_is_isa",
-    "what_is_dma"
-]
+    xhr.open('GET', url, true);
 
-if (webpageList.length != fileList.length) {
-    console.error("Invalid page list length!");
-}
-else {
-    for (let i = 0; i < webpageList.length; i++) {
-        document.write('<li><a href="' + fileList[i] + '.html">' + webpageList[i] + '</a></li>')
-    } 
-}
+    xhr.onload = function() {
+        const data = JSON.parse(this.response);
+        console.log(data);
+    };
+    
+    xhr.send();
+
+    for (let i = 0; i < data.length; i++) {
+        document.write('<li><a href="' + data + '.html">' + data[i] + '</a></li>')
+    }
+
+// var webpageList = [
+//     "Quest to Build my First Windows 98 PC",
+//     "What is ISA?",
+//     "What is DMA?"
+// ]
+
+// var fileList = [
+//     "quest_to_build_my_first_windows_98_pc",
+//     "what_is_isa",
+//     "what_is_dma"
+// ]
+
+// if (webpageList.length != fileList.length) {
+//     console.error("Invalid page list length!");
+// }
+// else {
+//     for (let i = 0; i < webpageList.length; i++) {
+//         document.write('<li><a href="' + fileList[i] + '.html">' + webpageList[i] + '</a></li>')
+//     } 
+// }

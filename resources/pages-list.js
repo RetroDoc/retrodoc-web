8528b6ad29dab0dfae65c438480d23a0667604f2
TYX8926 <139226237+TYX8926@users.noreply.github.com>
2025-09-30 22:34:14 +0800

    Automated pages list created

--- resources/pages-list.js
+++ resources/pages-list.js
@@ -1,3 +1,8 @@
+title = [
+    "<h3>All articles</h3>",
+    "<p>You can find all of the published articles here.</p>"
+]
+
 const xhr = new XMLHttpRequest();
     const url = "https://api.github.com/repositories/1039382278/contents/pages"
     // Replace -username- with your GitHub username, -repo- with the repository name, and then :path with a path to the file or folder you want to get the content of (leave blank to ge all files of the repository)
@@ -9,29 +14,8 @@ const xhr = new XMLHttpRequest();
         console.log(data);
 
         for (let i = 0; i < data.length; i++) {
-            document.write('<li><a href="' + data[i].name + '.html">' + data[i].name + '</a></li>')
+            document.getElementById("pages-list").innerHTML('<li><a href="' + data[i].name + '.html">' + data[i].name + '</a></li>')
         }
     };
     
     xhr.send();
-
-// var webpageList = [
-//     "Quest to Build my First Windows 98 PC",
-//     "What is ISA?",
-//     "What is DMA?"
-// ]
-
-// var fileList = [
-//     "quest_to_build_my_first_windows_98_pc",
-//     "what_is_isa",
-//     "what_is_dma"
-// ]
-
-// if (webpageList.length != fileList.length) {
-//     console.error("Invalid page list length!");
-// }
-// else {
-//     for (let i = 0; i < webpageList.length; i++) {
-//         document.write('<li><a href="' + fileList[i] + '.html">' + webpageList[i] + '</a></li>')
-//     } 
-// }

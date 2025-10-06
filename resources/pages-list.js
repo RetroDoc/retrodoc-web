8e9d38cee49ad2a93a61924c9e6b9f6707df4907
TYX8926 <139226237+TYX8926@users.noreply.github.com>
2025-10-06 20:07:05 +0800

    Trying to fix JSON parsing through URL

--- resources/pages-list.js
+++ resources/pages-list.js
@@ -1,4 +1,5 @@
-const data = JSON.parse("pages-list.json");
+import data from "https://raw.githubusercontent.com/RetroDoc/retrodoc-web/refs/heads/main/resources/pages-list.json" assert { type: 'json' };
+// const data = JSON.parse('pages-list.json');
 
 for (var ctv = 0; ctv < data.length; ctv++) {
 
